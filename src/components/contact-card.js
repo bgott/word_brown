4c989e49e0b964053896fe69eee7fd79fde6d622
Brandon Gottfried <bgott@stanford.edu>
2020-01-18 11:04:58 -0800

    top row of group info

--- src/components/contact-card.js
+++ src/components/contact-card.js
@@ -1,20 +1,68 @@
 import React from 'react'
 import Card from '@material-ui/core/Card';
-//import { makeStyles } from '@material-ui/core/styles';
-
-// const useStyles = makeStyles({
-//     card: {
-//         minWidth:500;
-//         minHeight:500;
-//     },
-// })
+import UserIcon from '@material-ui/icons/Person';
+import PhoneIcon from '@material-ui/icons/Phone';
+import PinIcon from '@material-ui/icons/LocationOn';
+import EmailIcon from '@material-ui/icons/Email';
 
 const ContactCard = () => (
     <>
-        <div className="contact-card">
+        <div className="contact-section">
             <h3>Group Information</h3>
             <Card elevation="4" className="card">
-                <div className="poop">
+                <div className="row">
+                    <div className="contact-field">
+                        <div className="icon">
+                            <UserIcon style={{fontSize:42}}/>
+                        </div>
+                        <div className="data">
+                            <div className="contact-field-title">
+                                Contact Person
+                            </div>
+                            <div className="contact-field-value">
+                                Dinesh Chugtai
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contact-field">
+                        <div className="icon">
+                            <PhoneIcon style={{fontSize:35}}/>
+                        </div>
+                        <div className="data">
+                            <div className="contact-field-title">
+                                Phone
+                            </div>
+                            <div className="contact-field-value">
+                                (213) 555-1145
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contact-field">
+                        <div className="icon">
+                            <EmailIcon style={{fontSize:30}}/>
+                        </div>
+                        <div className="data">
+                            <div className="contact-field-title">
+                                Email
+                            </div>
+                            <div className="contact-field-value">
+                                DChugtai@piedpiper.com
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contact-field">
+                        <div className="icon">
+                            <PinIcon style={{fontSize:35}}/>
+                        </div>
+                        <div className="data">
+                            <div className="contact-field-title">
+                                Zip Code
+                            </div>
+                            <div className="contact-field-value">
+                                92612
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </Card>
         </div>
