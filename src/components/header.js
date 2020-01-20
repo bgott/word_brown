1e2b907a9930ad029ec62250c84715214760da97
Brandon Gottfried <bgott@stanford.edu>
2020-01-20 11:26:02 -0800

    add logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import LogoSVG from "../assets/images/logo-white.svg"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -9,24 +10,8 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: '95%',
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+    <div className="header-bounds">
+        <img src={LogoSVG} alt={siteTitle}/>
     </div>
   </header>
 )
